64eaee876e48997fd2da4a161b826eadc126f697
Playnxbyt <S.Playnx@gmail.com>
2026-05-23 12:59:28 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'evolve-cache-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            // Return cached file if found, otherwise fetch from network
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
