13a4f58c1a5774007a27f4a42707fdbdd7c43874
Playnxbyt <S.Playnx@gmail.com>
2026-05-30 12:13:56 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'evolve-cache-v7';
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './Icon-192.png',
+  './icon-512.png'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate service worker and delete old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => clients.claim())
+  );
+});
+
+// Fetch files
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Clone response
+        const responseClone = response.clone();
+
+        // Store updated file in cache
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        // If offline, return cached version
+        return caches.match(event.request);
+      })
+  );
+});
